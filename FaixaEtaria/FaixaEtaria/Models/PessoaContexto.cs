a7f144ae4af28368f0ed1cce3fb567b977f1f370
Thiago Paiva  Medeiros <thiagopaivamed@gmail.com>
2019-03-19 21:14:36 -0300

    Partial Views

---
+++ FaixaEtaria/FaixaEtaria/Models/PessoaContexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaixaEtaria.Models
+{
+    public class PessoaContexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        public PessoaContexto(DbContextOptions<PessoaContexto> opcoes) : base (opcoes)
+        {
+
+        }
+    }
+}
